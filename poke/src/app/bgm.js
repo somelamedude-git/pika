b285faa1f1397bb1e6c24a586943559caa081cfb
somelamedude-git <cozydeveloper843@gmail.com>
2025-07-05 12:42:08 +0530

    Background music configured with howler

--- poke/src/app/bgm.js
+++ poke/src/app/bgm.js
@@ -1,1 +1,21 @@
-import { Howl}
+"use client";
+import { Howl, Howler } from 'howler';
+import { useRef, useEffect } from "react";
+
+export default function BackgroundMusic(vol){
+    const musicRef = useRef(null);
+
+    useEffect(()=>{
+        const music = new Howl({
+            src: '/pokeMusic.mp3',
+            loop:true,
+            volume: vol
+        })
+        music.play();
+        return()=>music.unload();
+    }, []);
+
+    useEffect(()=>{
+        Howler.volume(vol);
+    }, [vol]) //We don't want to recreate the instances
+}
